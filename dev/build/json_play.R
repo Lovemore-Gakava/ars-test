99caf3d8b88cab747e34a635b17f1dcbc595e57d
LVGK (Lovemore Gakava) <lvgk@novonordisk.com>
2025-03-07 18:40:55 +0100

    undo changes to original ars_ldm.yaml.

--- dev/build/json_play.R
+++ dev/build/json_play.R
@@ -4,3 +4,6 @@ data_change_simple <- fromJSON("dev/build/data_change_simple_sub_added.json")
 ARS_V1_SAF <- fromJSON("dev/examples/ARS_V1_Common_Safety_Displays.json")
 data_change_simple$analysisSets
 ARS_V1_SAF$analyses
+
+
+ars <- fromJSON("dev/build/ars_ldm.json")
