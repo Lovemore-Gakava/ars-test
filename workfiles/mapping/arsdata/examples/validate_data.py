7ffef82af3c647e63556dc8a68e87e752e8095e3
LVGK (Lovemore Gakava) <lvgk@novonordisk.com>
2025-03-17 10:10:56 +0100

    add content on mapping

---
+++ workfiles/mapping/arsdata/examples/validate_data.py
@@ -0,0 +1,3 @@
+# linkml-validate -s ../../schema/ars_ldm.yaml excel/csd_reimagined.json
+# linkml-validate -s ../../schema/ars_ldm.yaml excel/tfldesign.json
+# linkml-validate -s ../../schema/ars_ldm.yaml excel/llmdesign.json
